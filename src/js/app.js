82524e140310cebdf7edfda3cdbd02db3b4f0fc3
Hector Ector <163536279+AmrHector@users.noreply.github.com>
2024-06-04 22:54:51 +0000

    Updated this and ready to submit

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import "../style/index.css";
-
+console.log(window.variables);
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
@@ -11,12 +11,12 @@ import "../style/index.css";
         socialMediaPosition: "right", // social media bar position (left or right)
         
         twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
+        github: AmrsHector,
+        linkedin: Herbert Ector,
         instagram: null,
 
-        name: null,
-        lastName: null,
+        name: Herbert,
+        lastName: Ector,
         role: null,
         country: null,
         city: null
@@ -33,14 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -51,24 +51,20 @@ function render(variables = {}) {
  */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should show the cover image
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
+    avatarURL:
+      "https://media.licdn.com/dms/image/D4E03AQEKPnUtsfvxXw/profile-displayphoto-shrink_200_200/0/1702494173842?e=1723075200&v=beta&t=53YwitYx3kwqEwFJdhijYsy4X6GkERcE14vKQw-kyGs",
     socialMediaPosition: "position-left",
-    // social media usernames
     twitter: null,
-    github: null,
-    linkedin: null,
+    github: "AmrsHector",
+    linkedin: "Herbert Ector",
     instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    name: "Herbert",
+    lastName: "Ector",
+    role: "Supply Chain",
+    country: "United States",
+    city: "Richmond"
   };
   render(window.variables); // render the card for the first time
 
